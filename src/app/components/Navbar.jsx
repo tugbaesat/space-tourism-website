90c3a9452a366f9a9765f3950094340dac3c87a0
Tugba Esat <tugbaesat007@gmail.com>
2023-10-28 21:36:45 -0700

    navbar desktop added

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -8,11 +8,33 @@ const Navbar = () => {
   return (
     <section className="flex justify-between align-baseline px-8 py-4">
       <div>
-        <Image src={logo} alt="logo" width={40} height={40}/>
+        <Image src={logo} alt="logo" width={40} height={40} />
       </div>
       <div>
-        <Image src={iconHamburger} alt="logo" width={21} height={24}/>
-        <Image className="hidden" src={iconClose} alt="logo" width={40} height={40}/>
+        <ul className="flex gap-4">
+          <li className="uppercase">
+            <span className="font-bold">00</span> Home
+          </li>
+          <li className="uppercase">
+            <span className="font-bold">01</span> Destination
+          </li>
+          <li className="uppercase">
+            <span className="font-bold">02</span> Crew
+          </li>
+          <li className="uppercase">
+            <span className="font-bold">03</span> Technology
+          </li>
+        </ul>
+      </div>
+      <div>
+        <Image src={iconHamburger} alt="logo" width={21} height={24} />
+        <Image
+          className="hidden"
+          src={iconClose}
+          alt="logo"
+          width={40}
+          height={40}
+        />
       </div>
     </section>
   );
